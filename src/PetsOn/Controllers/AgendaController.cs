b739f7024c22d384807fa4806011646eeda58488
Gabriel Nakhata <gabrielnakata@gmail.com>
2022-12-06 22:14:23 -0300

    fullcalendar inicio da implementação da função agenda

---
+++ src/PetsOn/Controllers/AgendaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetsOn.Repository.DAL;
+using PetsOn.Services.Interfaces;
+using PetsOn.Models;
+using PetsOn.Helpers;
+
+namespace PetsOn.Controllers
+{
+    public class AgendaController : Controller
+    {
+        protected IHttpContextAccessor HttpContextAcessor;
+        public AgendaController(
+            IHttpContextAccessor httpContextAcessor)
+        {
+            HttpContextAcessor = httpContextAcessor;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/PetsOn/Models/AgendaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PetsOn.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetsOn.Models
+{
+    public class AgendaViewModel
+
+    {
+        public int? Codigo_Agenda { get; set; }
+
+      
+    }
+}
